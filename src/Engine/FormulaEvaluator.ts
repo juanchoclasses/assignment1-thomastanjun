527118eb4e5004d56e08f71bf845593091701716
Jun <thomastanjun@gmail.com>
2023-09-30 00:51:21 -0700

    updated calculation process in evaluate

--- src/Engine/FormulaEvaluator.ts
+++ src/Engine/FormulaEvaluator.ts
@@ -52,20 +52,62 @@ export class FormulaEvaluator {
     let numberCount = 0;
 
     let messageIndicator = 1;
+    const operators: string[] = [];
+    const values: number[] = [];
 
+    function calculate(){
+      const operator = operators.pop();
+      const left: number = values.pop()!;
+      const right: number = values.pop()!;
 
+      switch(operator){
+        case "+":
+          values.push(left + right);
+          break;
+        case "-":
+          values.push(right - left);
+          break;
+        case "*":
+          values.push(left * right);
+          break;
+        case "/":
+          if (left == 0){
+            messageIndicator = 8;
+            break;
+          }
+          values.push(right / left);
+          break;
+      }
+    }
 
     for (let i = 0; i < formula.length; i++) {
       let token = formula[i];
 
       if (token == "(") {
+        if (i != 0) {
+          let prevtoken = formula[i - 1];
+          if (this.isOperator(prevtoken) != true && prevtoken != "(") {
+            messageIndicator = 12;
+            break;
+          }
+        }
         parenthesesCount++; 
+        operators.push(token);
       }
       else if (token == ")") {
         if (parenthesesCount == 0) {
           messageIndicator = 13;
           break;
         }
+        else if (formula[i-1] == "(") {
+          parenthesesCount--;
+          messageIndicator = 13;
+          break;
+        }
+        while (operators[operators.length - 1] != "(") {
+          calculate();
+        }
+        operators.pop();
         parenthesesCount--;
       }
       else if (this.isOperator(token) == true) {
@@ -81,6 +123,11 @@ export class FormulaEvaluator {
             messageIndicator = 12;
             break;
           }
+          if ((token == '+' || token == '-' && operators.length > 0) && (operators[operators.length - 1] == '*'
+            || operators[operators.length - 1] == '/')) {
+            calculate();
+          }
+          operators.push(token);
         }
       }
       else if (formula.length > 1 && this.isCellReference(token) == true) {
@@ -104,6 +151,13 @@ export class FormulaEvaluator {
             break;
           }
         }
+        values.push(this.getCellValue(token)[0]);
+      }
+      else if (formula.length == 1 && this.isCellReference(token) == true) {
+        values.push(this.getCellValue(token)[0]);
+      }
+      else if (this.isNumber(token) == true) {
+        values.push(Number(token));
       }
     }
     
@@ -115,7 +169,21 @@ export class FormulaEvaluator {
       messageIndicator = 13;
     }
 
-    this._result = formula.length;
+    while (operators.length > 0) {
+      calculate();
+    }
+    
+    if (values.length == 1) {
+      this._result = values.pop()!;
+    }
+    else {
+      this._result = 0;
+    }
+
+    if (parenthesesCount == 0 && messageIndicator == 13) {
+      this._result = 0;
+    }
+    
     this._errorMessage = "";
 
     switch (messageIndicator) {
@@ -127,6 +195,7 @@ export class FormulaEvaluator {
         break;
       case 8:
         this._errorMessage = ErrorMessages.divideByZero;
+        this._result = Infinity;
         break;
       case 9:
         this._errorMessage = ErrorMessages.invalidCell;
